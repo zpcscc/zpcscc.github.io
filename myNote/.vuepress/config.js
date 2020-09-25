bf3fc2c2fbebefdd721ef32cfec5cf665ef23cfc
zhupengcheng <1071667708@qq.com>
2020-09-25 21:45:25 +0800

    更新目录

--- myNote/.vuepress/config.js
+++ myNote/.vuepress/config.js
@@ -50,8 +50,10 @@ module.exports = {
             {
                 text: '开发',
                 items: [
+                    { text: 'AntDesign', link: '/开发/AntDesign/' },
                     { text: 'Git', link: '/开发/Git/' },
                     { text: 'GitBook', link: '/开发/GitBook/' },
+                    { text: 'Nginx', link: '/开发/Nginx/' },
                 ]
             },
             {
@@ -68,43 +70,51 @@ module.exports = {
         ],  // 导航栏目录设置
         sidebar: {
             '/前端/HTML/': [
-                '', 
+                '',
                 'HTML-标签'
             ],
             '/前端/CSS/': [
-                '', 
-                'CSS-画三角形', 
-                'CSS-布局：元素垂直居中', 
-                'CSS-布局：元素水平居中', 
+                '',
+                'CSS-画三角形',
+                'CSS-布局：元素垂直居中',
+                'CSS-布局：元素水平居中',
                 'CSS-布局：元素水平垂直居中',
             ],
             '/前端/JavaScript/': [
-                '', 
-                'JavaScript-数据类型', 
-                'JavaScript-变量', 
-                'JavaScript-函数', 
-                'JavaScript-三大结构', 
-                'JavaScript-数组', 
-                'JavaScript-数组方法详解', 
-                'JavaScript-运算符', 
-                'JavaScript-运行机制', 
-                'JavaScript-arguments', 
+                '',
+                'JavaScript-数据类型',
+                'JavaScript-变量',
+                'JavaScript-函数',
+                'JavaScript-三大结构',
+                'JavaScript-数组',
+                'JavaScript-数组方法详解',
+                'JavaScript-运算符',
+                'JavaScript-运行机制',
+                'JavaScript-arguments',
                 'JavaScript-浏览器兼容',
             ],
             '/前端/分享/': [
-                '', 
-                '优雅降级与渐进增强', 
+                '',
+                '优雅降级与渐进增强',
                 '页面性能优化的简单介绍'
             ],
+            '/开发/AntDesign/': [
+                '',
+                'Antd-修改组件内部样式'
+            ],
             '/开发/Git/': [
-                '', 
-                'Git-新建关联克隆仓库', 
+                '',
+                'Git-新建关联克隆仓库',
                 'Git-强制切换分支',
                 'Git-强制拉取最新代码',
                 'Git-终端查看提交记录',
             ],
             '/开发/GitBook/': [
                 '',
+            ],
+            '/开发/Nginx/': [
+                '',
+                'Nginx-常用命令'
             ],
             '/日常/日记/': [
                 '',
